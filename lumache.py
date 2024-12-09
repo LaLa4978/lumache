1a8a6a8b0629cde12892cbbbf98acd526f09e797
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-09 17:57:04 +0900

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -12,10 +12,11 @@ class InvalidKindError(Exception):
 def get_random_ingredients(kind=None):
     """
     :param function: 수학적 표현으로 입력된 함수
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
+    :type function: str
+    :param center: 테일러 급수의 전개 중심
+    :type center: int
+    :param orders: 테일러 급수의 차수 목록
+    :type orders: list[int]
 
     """
     return ["shells", "gorgonzola", "parsley"]
